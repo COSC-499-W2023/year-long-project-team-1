2cdec383e66d7c3c1480cbdafb98d65b29e59ae7
connor@connordoman.dev <connor@connordoman.dev>
2023-10-22 18:37:04 -0700

    added session provider

--- app/front-end/src/app/layout.tsx
+++ app/front-end/src/app/layout.tsx
@@ -1,5 +1,7 @@
-import "./globals.css";
 import type { Metadata } from "next";
+import NextAuthProvider from "@components/auth/NextAuthProvider";
+
+import "./globals.css";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -12,7 +14,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
-            <body className={inter.className}>{children}</body>
+            <body className={inter.className}>
+                <NextAuthProvider>{children}</NextAuthProvider>
+            </body>
         </html>
     );
 }
