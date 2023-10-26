13f80ccbbd5463c70ec1934df1052aedd0ea34ca
MyStackOverflows <piunger@hotmail.com>
2023-10-26 16:05:59 -0700

    feat(back-end): updated file routing for server.py

--- app/back-end/video-processing/server.py
+++ app/back-end/video-processing/server.py
@@ -7,14 +7,13 @@ vp = VideoProcessor()
 
 @app.route("/process_video", methods=["POST"])
 def handle_request():
-    path = os.environ.get("PRIVACYPAL_VIDEO_DIRECTORY", "/opt/privacypal/videos") # default to /opt/privacypal/videos
+    path = os.environ.get("PRIVACYPAL_VIDEO_DIRECTORY", "/opt/privacypal/videos/input_videos") # default to /opt/privacypal/videos/input_videos
     if request.method == "POST":
-        file = request.data.decode("utf-8")     # expects just the filename, such as "sample_2023-10-24.mp4"
+        file = request.data.decode("utf-8")     # expects the filename, in the form <uid>-<file name>-<epoch time> such as "23-yeehaw-1698360721.mp4"
         if os.path.isfile(f"{path}/{file}"):    # check if the file exists
-            out = f"{path}/{file[:-4]}"
-            if not os.path.isdir(out):
-                os.mkdir(out)   # if the directory doesn't exist, make a directory with the name of the file we're going to be processing
-            process = mp.Process(target=vp.process_INTERPOLATE, args=(f"{path}/{file}", f"{out}/temp.mp4", f"{out}/final.mp4", ))  # define a new process pointing to process_INTERPOLATE
+            out = f"{path}/../output_videos/{file[:-4]}-processed"  # apparently relative paths are a thing that work
+            tmp, final = f"{out}-temp{file[-4:]}", f"{out}-final{file[-4:]}"
+            process = mp.Process(target=vp.process_INTERPOLATE, args=(f"{path}/{file}", tmp, final, ))  # define a new process pointing to process_INTERPOLATE
             process.start() # start the process on another thread
             print(f"Process started on {file}")
             return "Success: file exists"
