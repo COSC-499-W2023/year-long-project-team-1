234fc17404581aca80d496065cfc0527c7ffe07b
MyStackOverflows <piunger@hotmail.com>
2023-10-26 16:13:40 -0700

    feat(back-end): delete temp processed file when no longer needed

--- app/back-end/video-processing/server.py
+++ app/back-end/video-processing/server.py
@@ -12,7 +12,7 @@ def handle_request():
         file = request.data.decode("utf-8")     # expects the filename, in the form <uid>-<file name>-<epoch time> such as "23-yeehaw-1698360721.mp4"
         if os.path.isfile(f"{path}/{file}"):    # check if the file exists
             out = f"{path}/../output_videos/{file[:-4]}-processed"  # apparently relative paths are a thing that work
-            tmp, final = f"{out}-temp{file[-4:]}", f"{out}-final{file[-4:]}"
+            tmp, final = f"{out}-temp{file[-4:]}", f"{out}{file[-4:]}"
             process = mp.Process(target=vp.process_INTERPOLATE, args=(f"{path}/{file}", tmp, final, ))  # define a new process pointing to process_INTERPOLATE
             process.start() # start the process on another thread
             print(f"Process started on {file}")
