40a30161897c319f4da5db1cff871aa60aefcf3c
MyStackOverflows <piunger@hotmail.com>
2023-10-26 17:49:45 -0700

    feat(back-end): implement review comments

--- app/back-end/video-processing/entrypoint.sh
+++ app/back-end/video-processing/entrypoint.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
-if [ -d $PRIVACYPAL_INPUT_VIDEO_DIR ] && [ -d $PRIVACYPAL_OUT_VIDEO_DIR ]; then
-    python3 -m gunicorn server:app
-else
+if [ ! -d $PRIVACYPAL_INPUT_VIDEO_DIR ] || [ ! -d $PRIVACYPAL_OUT_VIDEO_DIR ]; then
     echo Our in/out directories do not exist, exiting...
+    exit
 fi
+python3 -m gunicorn server:app
