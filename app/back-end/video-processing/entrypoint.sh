ce2559d1a72b1b44dfd53463ab3bc6e1e8d76511
MyStackOverflows <piunger@hotmail.com>
2023-10-26 18:27:04 -0700

    feat(back-end): implement review comments v5

--- app/back-end/video-processing/entrypoint.sh
+++ app/back-end/video-processing/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -xe
-export PRIVACYPAL_INPUT_VIDEO_DIR="/opt/privacypal/videos/input_videos"
-export PRIVACYPAL_OUT_VIDEO_DIR="/opt/privacypal/videos/out_video"
-if [ ! -d $PRIVACYPAL_INPUT_VIDEO_DIR ] || [ ! -d $PRIVACYPAL_OUT_VIDEO_DIR ]; then
-    echo "[SEVERE][$(date)]: $PRIVACYPAL_INPUT_VIDEO_DIR and $PRIVACYPAL_OUTPUT_VIDEO_DIR must exist."
+export PRIVACYPAL_INPUT_VIDEO_DIR=${PRIVACYPAL_INPUT_VIDEO_DIR:-/opt/...}
+export PRIVACYPAL_OUT_VIDEO_DIR=${PRIVACYPAL_OUT_VIDEO_DIR:-/opt/...}
+if [ ! -d "$PRIVACYPAL_INPUT_VIDEO_DIR" ] || [ ! -d "$PRIVACYPAL_OUT_VIDEO_DIR" ]; then
+    echo "[SEVERE][$(date)]: $PRIVACYPAL_INPUT_VIDEO_DIR and $PRIVACYPAL_OUT_VIDEO_DIR must exist."
     exit 1
 fi
 exec python3 -m gunicorn server:app
