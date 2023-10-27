bda4768659d7cf57d240650968284eb023127a3e
MyStackOverflows <piunger@hotmail.com>
2023-10-26 18:29:30 -0700

    feat(back-end): implement review comments v6

--- app/back-end/video-processing/entrypoint.sh
+++ app/back-end/video-processing/entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 set -xe
+
 export PRIVACYPAL_INPUT_VIDEO_DIR=${PRIVACYPAL_INPUT_VIDEO_DIR:-/opt/...}
 export PRIVACYPAL_OUT_VIDEO_DIR=${PRIVACYPAL_OUT_VIDEO_DIR:-/opt/...}
+
 if [ ! -d "$PRIVACYPAL_INPUT_VIDEO_DIR" ] || [ ! -d "$PRIVACYPAL_OUT_VIDEO_DIR" ]; then
     echo "[SEVERE][$(date)]: $PRIVACYPAL_INPUT_VIDEO_DIR and $PRIVACYPAL_OUT_VIDEO_DIR must exist."
     exit 1
