a6eac561345b37cc857d968363e1b866bfa921b9
MyStackOverflows <piunger@hotmail.com>
2023-10-29 01:23:16 -0700

    feat(front-end): update video upload ui

--- app/front-end/src/app/video-upload/page.tsx
+++ app/front-end/src/app/video-upload/page.tsx
@@ -1,6 +1,15 @@
+import "./page.css";
 import { VideoUI } from "@components/UploadVideoForm";
+import { Text, TextContent, TextVariants } from "@patternfly/react-core";
 
 export function VideoUpload() {
-  return <VideoUI />;
+	return (
+		<main className="main">
+			<div className="title">
+				<h1>Upload a video!</h1>
+			</div>
+			<VideoUI />
+		</main>
+	);
 }
 export default VideoUpload;
