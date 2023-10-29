66067b34d95ca482cc97331d2be3b146ff5fd678
nganphan123 <jill01009@gmail.com>
2023-10-29 03:15:13 -0700

    update prisma client as global object

--- app/front-end/src/lib/db.ts
+++ app/front-end/src/lib/db.ts
@@ -1,5 +1,17 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client'
 
-const db = new PrismaClient();
+const prismaClientSingleton = () => {
+  return new PrismaClient()
+}
 
-export default db;
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined
+}
+
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
+
+export default prisma
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
