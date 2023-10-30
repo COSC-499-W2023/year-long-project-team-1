a070d3c6431935671c5d93279b8b6eede10bd451
MyStackOverflows <piunger@hotmail.com>
2023-10-29 20:35:16 -0700

    feat(tests): implement review comments

--- tests/back-end/README.md
+++ tests/back-end/README.md
@@ -2,6 +2,18 @@
 
 ## Requirements
  - Python 3.8
+    - boto3 v1.28.64
+    - botocore v1.31.64
+    - numpy v1.24.4
+    - opencv-python-headless v4.8.1.78
+    - flask v3.0.0
+    - gunicorn v21.2.0
+
+You can installl these dependencies by navigating to `app/back-end/video-processing` and running:
+```bash
+pip install -r requirements.txt
+```
+
 
 ## Running tests
 Navigate to the repository's root directory and run:
--- tests/back-end/test_server.py
+++ tests/back-end/test_server.py
@@ -12,39 +12,50 @@ from server import app  # finally, import our flask server
 
 class ServerTest(unittest.TestCase):
     app: Flask
-    method_not_allowed = b'<!doctype html>\n<html lang=en>\n<title>405 Method Not Allowed</title>\n<h1>Method Not Allowed</h1>\n<p>The method is not allowed for the requested URL.</p>\n'
 
     def setUp(self):
         self.app = app
         self.app.testing = True
+        self.client = self.app.test_client()
     
-    def test_process_video(self):
+    def test_process_video_file_not_found(self):
         route = "/process_video"
-        with self.app.test_client() as c:
-            for method in [c.get, c.put, c.delete, c.trace, c.patch]:
-                response = method(route)
-                self.assertEqual(self.method_not_allowed, response.data)
-                self.assertEqual(405, response.status_code)
-
+        with self.client as c:
             response = c.post(route, data="blahblahblah invalid file")
             self.assertEqual(b"Error: file not found", response.data)
             self.assertEqual(200, response.status_code)
 
+    def test_process_video_file_found(self):
+        route = "/process_video"
+        with self.client as c:
             response = c.post(route, data="1.mp4")
             self.assertEqual(b"Success: file exists", response.data)
             self.assertEqual(200, response.status_code)
 
-    def test_health(self):
-        route = "/health"
-        with self.app.test_client() as c:
-            for method in [c.put, c.post, c.delete, c.trace, c.patch]:
+    def test_process_video_method_not_allowed(self):
+        route = "/process_video"
+        with self.client as c:
+            for method in [c.get, c.put, c.delete, c.trace, c.patch]:
                 response = method(route)
-                self.assertEqual(self.method_not_allowed, response.data)
                 self.assertEqual(405, response.status_code)
 
+    def test_health(self):
+        route = "/health"
+        with self.client as c:
             response = c.get(route)
-            self.assertEqual(b"{}\n", response.data)
+            # response data = b"{someKey:someValue, someKey2: someValue2}",
+            # to convert to python dict and check values we convert to string,
+            # cut off the binary 'b' and quotes and cast to dict
+            json = dict(str(response.data)[2:-1])
+            self.assertEqual({}, json)
             self.assertEqual(200, response.status_code)
+
+    def test_health_method_not_allowed(self):
+        route = "/health"
+        with self.client as c:
+            for method in [c.put, c.post, c.delete, c.trace, c.patch]:
+                response = method(route)
+                self.assertEqual(405, response.status_code)
 
 if __name__ == "__main__":
     unittest.main()
