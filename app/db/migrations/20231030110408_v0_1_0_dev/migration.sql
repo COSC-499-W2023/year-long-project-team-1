3fcf3f6a6f9ce386b781dd383e39edc2587c5a24
Thuan Vo <thuan.votann@gmail.com>
2023-10-30 07:05:26 -0400

    feat(makefile): makefile for db local dev

--- app/db/migrations/20231030110408_v0_1_0_dev/migration.sql
+++ app/db/migrations/20231030110408_v0_1_0_dev/migration.sql
@@ -1,11 +1,10 @@
 -- CreateTable
 CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
-    "password" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
     "email" TEXT NOT NULL,
+    "firstname" TEXT NOT NULL,
+    "lastname" TEXT NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
-
--- CreateIndex
-CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
