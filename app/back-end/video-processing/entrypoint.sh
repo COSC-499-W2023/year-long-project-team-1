1fb870b941206113c73f16dedc6e78217551a65c
Thuan Vo <thuan.votann@gmail.com>
2023-10-30 19:02:12 -0400

    chore(env-var): correct environment variable names

--- app/back-end/video-processing/entrypoint.sh
+++ app/back-end/video-processing/entrypoint.sh
@@ -3,10 +3,10 @@
 set -e
 
 export PRIVACYPAL_INPUT_VIDEO_DIR=${PRIVACYPAL_INPUT_VIDEO_DIR:-/opt/privacypal/input_videos}
-export PRIVACYPAL_OUT_VIDEO_DIR=${PRIVACYPAL_OUT_VIDEO_DIR:-/opt/privacypal/output_videos}
+export PRIVACYPAL_OUTPUT_VIDEO_DIR=${PRIVACYPAL_OUTPUT_VIDEO_DIR:-/opt/privacypal/output_videos}
 
-if [ ! -d "$PRIVACYPAL_INPUT_VIDEO_DIR" ] || [ ! -d "$PRIVACYPAL_OUT_VIDEO_DIR" ]; then
-    echo "[SEVERE][$(date)]: $PRIVACYPAL_INPUT_VIDEO_DIR and $PRIVACYPAL_OUT_VIDEO_DIR must exist."
+if [ ! -d "$PRIVACYPAL_INPUT_VIDEO_DIR" ] || [ ! -d "$PRIVACYPAL_OUTPUT_VIDEO_DIR" ]; then
+    echo "[SEVERE][$(date)]: $PRIVACYPAL_INPUT_VIDEO_DIR and $PRIVACYPAL_OUTPUT_VIDEO_DIR must exist."
     exit 1
 fi
 
