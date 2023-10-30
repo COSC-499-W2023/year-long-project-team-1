cebab635c2f4b180df249c5965339ba0ccd9416b
Connor Doman <domanc@student.ubc.ca>
2023-10-30 16:08:47 -0700

    tweak(nextjs): improve .env generator

--- app/front-end/generate_dev_env.sh
+++ app/front-end/generate_dev_env.sh
@@ -4,7 +4,7 @@
 {
     echo NEXTAUTH_SECRET="$(openssl rand -base64 32)"
     echo NEXTAUTH_URL="http://localhost:${PORT:-8081}"
-    echo PRIVACYPAL_INPUT_VIDEO_DIR="../back-end/video-processing/input"
-    echo PRIVACYPAL_OUTPUT_VIDEO_DIR="../back-end/video-processing/output"
+    echo PRIVACYPAL_INPUT_VIDEO_DIR="../back-end/video-processing/input-videos"
+    echo PRIVACYPAL_OUTPUT_VIDEO_DIR="../back-end/video-processing/output-videos"
     echo PRIVACYPAL_CONFIG_DIR="./conf"
 } > .env.local
