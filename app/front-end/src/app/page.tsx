5c8dbb9b78cf23b25dab9ffb022c4377a23d55d0
Connor Doman <domanc@student.ubc.ca>
2023-10-30 16:14:50 -0700

    tweak(layout): simplify homepage layout

--- app/front-end/src/app/page.tsx
+++ app/front-end/src/app/page.tsx
@@ -1,17 +1,12 @@
+import Link from "next/link";
 import styles from "./Home.module.css";
+import { Title } from "@patternfly/react-core";
 
 export default function Home() {
     return (
         <main className={styles.column}>
-            <h2 className={styles.subheading}>Test Login</h2>
-            <p>
-                You can login with the username &quot;<code>johnny@example.com</code>&quot; and password &quot;
-                <code>password</code>&quot;
-            </p>
-            <br />
-            <p>
-                You can visit <a href="/api/session">this link</a> to see a breakdown of your current session.
-            </p>
+            <h2>Welcome to PrivacyPal</h2>
+            <Link href="/login">Log in</Link>
         </main>
     );
 }
