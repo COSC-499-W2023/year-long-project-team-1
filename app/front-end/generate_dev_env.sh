b1210ba4430f1bf60ac9c5bf08b30906ac00e401
Connor Doman <domanc@student.ubc.ca>
2023-10-30 14:31:35 -0700

    fix(review): dev port, root gitignore

--- app/front-end/generate_dev_env.sh
+++ app/front-end/generate_dev_env.sh
@@ -1,6 +1,6 @@
 # Create `.env.local` file for development environment
-PORT=8081
-DEV_PORT="3000"
+# PORT=8080
+DEV_PORT=8081
 echo "NEXTAUTH_SECRET=$(openssl rand -base64 32)" > .env.local
 echo "NEXTAUTH_URL=http://localhost:${PORT:-$DEV_PORT}" >> .env.local
 echo "PRIVACYPAL_INPUT_VIDEO_DIR=\"../back-end/video-processing/input\"" >> .env.local
