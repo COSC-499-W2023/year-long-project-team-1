fdd8e86ab4079119108f89f32df683f307a3308a
Connor Doman <domanc@student.ubc.ca>
2023-10-30 14:54:23 -0700

    tweak(nextjs): improved .env generator script

--- app/front-end/generate_dev_env.sh
+++ app/front-end/generate_dev_env.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
+
 # Create `.env.local` file for development environment
-# PORT=8080
-DEV_PORT=8081
-echo "NEXTAUTH_SECRET=$(openssl rand -base64 32)" > .env.local
-echo "NEXTAUTH_URL=http://localhost:${PORT:-$DEV_PORT}" >> .env.local
-echo "PRIVACYPAL_INPUT_VIDEO_DIR=\"../back-end/video-processing/input\"" >> .env.local
-echo "PRIVACYPAL_OUTPUT_VIDEO_DIR=\"../back-end/video-processing/output\"" >> .env.local
-echo "PRIVACYPAL_CONFIG_DIR=\"${PWD}/conf\"" >> .env.local
+{
+    echo NEXTAUTH_SECRET="$(openssl rand -base64 32)"
+    echo NEXTAUTH_URL="http://localhost:${PORT:-8081}"
+    echo PRIVACYPAL_INPUT_VIDEO_DIR="../back-end/video-processing/input"
+    echo PRIVACYPAL_OUTPUT_VIDEO_DIR="../back-end/video-processing/output"
+    echo PRIVACYPAL_CONFIG_DIR="./conf"
+} > .env.local
