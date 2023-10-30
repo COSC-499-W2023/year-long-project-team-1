02c34ff11f5094faa06a3d67ff3bd6fa1f000621
MyStackOverflows <piunger@hotmail.com>
2023-10-30 00:14:20 -0700

    feat(back-end): remove unnecessary trailing comma

--- app/back-end/video-processing/server.py
+++ app/back-end/video-processing/server.py
@@ -13,7 +13,7 @@ def handle_request():
         if os.path.isfile(f"{input_path}/{file}"):    # check if the file exists
             final = f"{out_path}/{file[:-4]}-processed{file[-4:]}"
             if not app.testing: # if we're running Flask unit tests, don't run the video processing method
-                process = mp.Process(target=vp.process_INTERPOLATE, args=(f"{input_path}/{file}", final, ))  # define a new process pointing to process_INTERPOLATE
+                process = mp.Process(target=vp.process_INTERPOLATE, args=(f"{input_path}/{file}", final))  # define a new process pointing to process_INTERPOLATE
                 process.start() # start the process on another thread
                 print(f"Process started on {file}")
             return "Success: file exists"
