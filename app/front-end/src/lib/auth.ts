ae038edbd5dafe222489c5be6a254d1f51279951
Connor Doman <domanc@student.ubc.ca>
2023-10-29 19:25:52 -0700

    configurable auth providers

--- app/front-end/src/lib/auth.ts
+++ app/front-end/src/lib/auth.ts
@@ -5,7 +5,7 @@
 
 import type { NextAuthOptions, RequestInternal, User } from "next-auth";
 import CredentialsProvider, { CredentialsConfig } from "next-auth/providers/credentials";
-import { DummyAuthenticator, PrivacyPalDummyUser } from "./dummy-authenticator";
+import { DummyAuthenticator } from "./dummy-authenticator";
 
 export type PrivacyPalCredentialsRecord = Record<"password" | "email", string> | undefined;
 export type AuthRequest = Pick<RequestInternal, "body" | "query" | "headers" | "method">;
@@ -16,18 +16,28 @@ export interface PrivacyPalAuthenticator {
     authorize: (credentials: PrivacyPalCredentialsRecord, req: AuthRequest) => Promise<User | null>;
 }
 
-const privacyPalAuthenticator = new DummyAuthenticator();
+const buildAuthenticationProviders = () => {
+    const manager = (process.env.PRIVACYPAL_AUTH_MANAGER ?? "basic") as "basic" | "aws_cognito";
+
+    switch (manager) {
+        case "aws_cognito":
+        case "basic":
+            const dummyProvider = new DummyAuthenticator();
+
+            return CredentialsProvider({
+                name: dummyProvider.name,
+                credentials: dummyProvider.credentials,
+                authorize: dummyProvider.authorize,
+            });
+        default:
+            throw new Error(`Invalid authentication manager: ${manager}`);
+    }
+};
 
 export const privacyPalAuthOptions: NextAuthOptions = {
     session: {
         strategy: "jwt",
     },
-    providers: [
-        CredentialsProvider({
-            name: privacyPalAuthenticator.name,
-            credentials: privacyPalAuthenticator.credentials,
-            authorize: privacyPalAuthenticator.authorize,
-        }),
-    ],
+    providers: [buildAuthenticationProviders()],
     secret: process.env.NEXTAUTH_SECRET ?? "",
 };
