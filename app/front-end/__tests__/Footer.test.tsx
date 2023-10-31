c265b70aa44e36570058485b5601962287b9a30d
Connor Doman <domanc@student.ubc.ca>
2023-10-30 18:37:00 -0700

    feat(tests): Footer component tests

--- app/front-end/__tests__/Footer.test.tsx
+++ app/front-end/__tests__/Footer.test.tsx
@@ -2,3 +2,41 @@
  * Created on Mon Oct 30 2023
  * Author: Connor Doman
  */
+
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import { Footer } from "@components/Footer";
+
+describe("Footer", () => {
+    it("matches snapshot", () => {
+        const wrapper = render(<Footer />);
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it("contains an HTML footer element", () => {
+        render(<Footer />);
+        expect(screen.getByRole("contentinfo")).toBeInTheDocument();
+    });
+
+    it("renders the site logo", () => {
+        render(<Footer />);
+        const logo = screen.getByRole("link", { name: "PrivacyPal logo" });
+        expect(logo).toBeInTheDocument();
+        expect(logo).toHaveAttribute("href", "/");
+    });
+
+    it("renders the site links", () => {
+        render(<Footer />);
+        expect(screen.getByRole("link", { name: "Welcome" })).toBeInTheDocument();
+        expect(screen.getByRole("link", { name: "About Us" })).toBeInTheDocument();
+        expect(screen.getByRole("link", { name: "Sign Up" })).toBeInTheDocument();
+        expect(screen.getByRole("link", { name: "Log in" })).toBeInTheDocument();
+    });
+
+    it("renders social media links", () => {
+        render(<Footer />);
+        expect(screen.getByRole("link", { name: "GitHub logo" })).toBeInTheDocument();
+        expect(screen.getByRole("link", { name: "YouTube logo" })).toBeInTheDocument();
+        expect(screen.getByRole("link", { name: "mail icon" })).toBeInTheDocument();
+    });
+});
