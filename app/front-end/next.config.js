0b5b4594f6b0ea6db1d95ca6ffe57f18a1c4acc6
Thuan Vo <thuan.votann@gmail.com>
2023-11-03 23:11:12 -0400

    fix(prisma): ensure ORM is initialized at build time

--- app/front-end/next.config.js
+++ app/front-end/next.config.js
@@ -2,9 +2,6 @@
 const nextConfig = {
     // https://nextjs.org/docs/pages/api-reference/next-config-js/output#automatically-copying-traced-files
     output: "standalone",
-    experimental: {
-      serverActions: true,
-    },
 };
 
 module.exports = nextConfig;
