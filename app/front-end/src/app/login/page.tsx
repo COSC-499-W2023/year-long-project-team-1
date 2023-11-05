6fe6836eed6b3b36355877a6ae282f587a137505
Connor Doman <domanc@student.ubc.ca>
2023-11-05 12:53:10 -0800

    feat(auth): basic auth requests parsed

--- app/front-end/src/app/login/page.tsx
+++ app/front-end/src/app/login/page.tsx
@@ -7,10 +7,17 @@
 import LoginFlow from "@components/auth/LoginFlow";
 import React from "react";
 
-export default function LoginPage() {
+export default function LoginPage({
+    searchParams,
+}: {
+    searchParams: { [key: string]: string | string[] | undefined };
+}) {
+    // get redirect url from query params
+    const redirectUrl = searchParams.r as string | undefined;
+
     return (
         <main>
-            <LoginFlow redirectUrl="/video-upload" />
+            <LoginFlow redirectUrl={redirectUrl ? decodeURIComponent(redirectUrl) : undefined} />
         </main>
     );
 }
--- app/front-end/src/app/page.tsx
+++ app/front-end/src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
             <h2>Welcome to PrivacyPal</h2>
             <Link href="/login">Log in</Link>
             <Link href="/staff">Staff Area</Link>
-            <Link href="/users">User Area</Link>
+            <Link href="/user">User Area</Link>
         </main>
     );
 }
