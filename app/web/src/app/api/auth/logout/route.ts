60dc0c630ceec7c12722f0a1680944e9f0b5fc7a
Connor Doman <domanc@student.ubc.ca>
2023-11-10 00:17:20 -0800

    fix(deps): update dependencies

--- app/web/src/app/api/auth/logout/route.ts
+++ app/web/src/app/api/auth/logout/route.ts
@@ -3,15 +3,10 @@
  * Author: Connor Doman
  */
 
-import { clearSession, getSession } from "@lib/session";
+import { clearSession } from "@lib/session";
 import { redirUrlFromReq } from "@lib/url";
 
 export async function GET(req: Request) {
-    const session = await getSession();
-
-    if (session) {
-        await clearSession();
-    }
-
+    await clearSession();
     return Response.redirect(redirUrlFromReq(req, "/"), 302);
 }
