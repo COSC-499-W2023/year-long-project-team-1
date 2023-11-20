dc1fc4bc00cf8c46c6b2526800d31a9261653dbf
nganphan123 <jill01009@gmail.com>
2023-11-19 22:32:39 -0800

    add update user data api

--- app/web/src/app/api/users/route.ts
+++ app/web/src/app/api/users/route.ts
@@ -1,4 +1,5 @@
 import db from "@lib/db"
+import { JsonObject } from "@prisma/client/runtime/library"
 import { NextRequest, NextResponse } from "next/server"
 
 
@@ -7,13 +8,13 @@ import { NextRequest, NextResponse } from "next/server"
   The user list returned is filtered based on ids. 
   If id param is not provided, the api will return all users.
 */
-const userQuery = (ids?: number[]) => db.user.findMany({
+const getusrDatQuery = (ids?: number[]) => db.user.findMany({
   where: {id: {in: ids || undefined}}
 })
 
 async function getUsrData(ids?: number[]) {
-  let data = await userQuery(ids)
-  return {status: 200, result: {data}} as const
+  let data = await getusrDatQuery(ids)
+  return {result: {data}} as const
 }
 
 export async function GET(req: NextRequest){
@@ -25,9 +26,9 @@ export async function GET(req: NextRequest){
   }
   try{
     let result = await getUsrData(usrIds);
-    return NextResponse.json(result)   
+    return NextResponse.json(result,{status: 200})   
   }catch(e){
-    return NextResponse.json({status: 400, message: e})
+    return NextResponse.json({message: e}, {status: 400})
   }
 }
 
@@ -36,3 +37,25 @@ function paramToIntList(param: string){
     return stringVals.map(v => Number(v))
 }
 
+// PUT api/users?id=1
+// UserId is required in parameters
+const updateUsrDatQuery = (id: number, data: JsonObject) => db.user.update({
+  where:{
+    id: id
+  },
+  data: data
+})
+export async function PUT(req: NextRequest) {
+  const searchParams = req.nextUrl.searchParams
+  const param = searchParams.get('id')
+  if (param === null) {
+    return NextResponse.json({message: "Missing user id"}, {status: 400});
+  }
+  try{
+    const data = await req.json()
+    let result = await updateUsrDatQuery(Number(param), data);
+    return NextResponse.json(result, {status: 200})   
+  }catch(e){
+    return NextResponse.json({message: e}, {status: 400})
+  }
+}
