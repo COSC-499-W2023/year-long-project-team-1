2a6c5ce506111c79b71a2bd20c52001d51dc2bb4
nganphan123 <jill01009@gmail.com>
2023-11-28 03:41:39 -0800

    base structure for process video api

--- app/web/src/app/api/video/upload/route.ts
+++ app/web/src/app/api/video/upload/route.ts
@@ -1,12 +1,7 @@
-/*
- * Created on Thu Oct 26 2023
- * Author: Connor Doman
- */
-
 import path from "path";
 import { writeFile } from "fs/promises";
 import { timeStampUTC } from "@lib/time";
-import { JSONResponse } from "@lib/json";
+import { NextResponse } from "next/server";
 
 const videosDirectory = process.env.PRIVACYPAL_INPUT_VIDEO_DIR || "/opt/privacypal/input_videos";
 
@@ -16,24 +11,59 @@ const allowedMimeTypes = [
     "video/quicktime", // mov
 ];
 
-export async function POST(req: Request) {
+export async function POST(req: Request){
     // retrieve user id, verify authenticated
     // this will require user auth to be verified
-    // TODO: link this with server-side user auth
+    // TODO: get userID from getSession()
     const userID = "12345678";
 
     // read the multipart/form-data
     const data = await req.formData();
     // get the file
-    const file: File | null = data.get("file") as unknown as File;
+    const file: File = data.get("file") as File;
 
     // if there was no file parameter, return 400 (bad request)
     if (!file) {
-        return Response.json({ success: false }, { status: 400 });
+        return Response.json({ message: "Video file is not found." }, { status: 400 });
     } else {
         console.log(`Next.js received file: ${file.name}`);
     }
 
+    if (!fileIsValid(file)){
+        return NextResponse.json({message: "Unsupported Media Type"}, {status: 415});
+    }
+
+    let filename: string
+    try {
+        filename = await saveVideo(file, userID)
+    }catch(err){
+        return Response.json({message: "Internal Server Error"}, {status: 500})
+    }
+
+    //TODO: extract to function
+    let vidUrl = "http://localhost:3000/process_video?"
+    let params = {
+        filename: filename,
+    }
+    console.log(vidUrl + new URLSearchParams(params), {
+        method: "POST",
+    })
+    let videoServerRes = await fetch(vidUrl + new URLSearchParams(params), {
+        method: "POST",
+    })
+    
+    if (videoServerRes.status == 202) {
+        return Response.json({message:"Video is being processed", filePath: filename}, {status: 200})
+    }else{
+        // TODO: call the clean file function
+        // TODO: print error message
+        // console.log(videoServerRes.body)
+        return Response.json({message: "Internal Server Error"}, {status: 500})
+    }
+
+}
+
+function fileIsValid(file: File): boolean{
     // extract the MIME type
     const fileMimeType = file.type;
 
@@ -41,10 +71,12 @@ export async function POST(req: Request) {
     // allowed MIME types are only common videos
     if (!allowedMimeTypes.includes(fileMimeType)) {
         console.log(`MIME type not allowed: ${fileMimeType}`);
-        const res: JSONResponse = { errors: [{ status: "415", title: "Unsupported Media Type" }] };
-        return Response.json(res, { status: 415 });
+        return false
     }
+    return true
+}
 
+async function saveVideo(file: File, userID: string): Promise<string> {
     // read the file into a buffer
     const bytes = await file.arrayBuffer();
     const buffer = Buffer.from(bytes);
@@ -52,21 +84,20 @@ export async function POST(req: Request) {
     // determine the path to write the file to
     const cwd = videosDirectory;
     const extension = path.extname(file.name);
+    // file name extracted from request
     const fileBaseName = path.basename(file.name, extension);
-    const filePath = path.join(cwd, `${userID}-${fileBaseName}-${timeStampUTC()}${extension}`);
+    // file name combined with userID and timestamp
+    const filename = `${userID}-${fileBaseName}-${timeStampUTC()}${extension}`;
+    const filePath = path.join(cwd, filename);
 
     try {
         // write file to disk
         // file will be overwritten if it exists
         await writeFile(filePath, buffer);
         console.log(`file uploaded to: ${filePath}`);
-
-        // return success with file path
-        const res: JSONResponse = { data: { success: true } };
-        return Response.json(res);
+        return filename
     } catch (err: any) {
         console.error(err);
-        const res: JSONResponse = { errors: [{ status: "500", title: "Internal Server Error" }] };
-        return Response.json(res, { status: 500 });
+        throw err
     }
 }
