0df429247a09243ab3ed3b8da5b9f7a0a5b93966
Connor Doman <domanc@student.ubc.ca>
2023-11-27 20:49:53 -0800

    fix(cypress): APP_PORT, add single newline

--- app/web/cypress.config.ts
+++ app/web/cypress.config.ts
@@ -4,13 +4,13 @@
  */
 import { defineConfig } from "cypress";
 
-const PORT = process.env.PORT ?? 8081;
+const APP_PORT = process.env.PORT ?? 8081;
 
 export default defineConfig({
     projectId: "tyhfus",
     e2e: {
         specPattern: "cypress/{tests,e2e}/**/*.cy.{js,jsx,ts,tsx}",
-        baseUrl: `http://localhost:${PORT}/`,
+        baseUrl: `http://localhost:${APP_PORT}/`,
         supportFile: false,
         setupNodeEvents(on, config) {
             // implement node event listeners here
