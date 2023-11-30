e0f03c15308dd0a4862af9084007ec4359de094a
Connor Doman <domanc@student.ubc.ca>
2023-11-30 00:24:16 -0800

    feat(style): global card styles

--- app/web/src/components/staff/NewAppointmentForm.tsx
+++ app/web/src/components/staff/NewAppointmentForm.tsx
@@ -7,11 +7,18 @@
 import { Card, CardBody, CardTitle, TextInput } from "@patternfly/react-core";
 import { useSearchParams, useRouter } from "next/navigation";
 
-export const NewAppointmentForm = () => {
+export const NewAppointmentForm = async () => {
     return (
         <Card component="form" action="">
             <CardTitle title="h1">New Appointment</CardTitle>
             <CardBody>
+                <TextInput
+                    aria-label="appt-name"
+                    name="appt-name"
+                    placeholder="Appointment Name"
+                    isRequired
+                    data-ouia-component-id="login_appointment_name"
+                />
                 <TextInput
                     aria-label="appt-name"
                     name="appt-name"
