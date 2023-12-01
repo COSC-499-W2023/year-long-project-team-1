86158fb4b89670e6c13cda684515eb47162cf2c8
linhnnk <linhnnk202@gmail.com>
2023-12-01 15:52:46 -0800

    add forms

--- app/web/src/components/user/UserUpdateForm.tsx
+++ app/web/src/components/user/UserUpdateForm.tsx
@@ -1,4 +1,4 @@
-// components/UserUpdateForm.tsx
+"use client"
 import React, { useState } from "react";
 import {
   Card,
@@ -6,135 +6,61 @@ import {
   CardTitle,
   TextInput,
   Button,
-  ValidatedOptions,
-  HelperText,
-  HelperTextItem,
   ActionList,
   ActionListItem,
 } from "@patternfly/react-core";
-import ExclamationCircleIcon from "@patternfly/react-icons/dist/esm/icons/exclamation-circle-icon";
-import Link from "next/link";
 import { User } from "@prisma/client";
+import Link from "next/link";
 
 interface UserUpdateFormProps {
   user: User;
-  email: string;
-  isPasswordForm?: boolean;
 }
 
-const UserUpdateForm: React.FunctionComponent<UserUpdateFormProps> = ({ user, email, isPasswordForm }: UserUpdateFormProps) => {
+const UserUpdateForm: React.FC<UserUpdateFormProps> = ({ user }: UserUpdateFormProps) => {
   const [showHelperText, setShowHelperText] = useState(false);
-  const [currentPassword, setCurrentPassword] = useState("");
-  const [newPassword, setNewPassword] = useState("");
-  const [confirmNewPassword, setConfirmNewPassword] = useState("");
-  const [isValidNewPassword, setIsValidNewPassword] = useState(true);
-  const [isValidConfirmNewPassword, setIsValidConfirmNewPassword] = useState(true);
+  const [firstname, setFirstName] = useState("");
+  const [lastname, setLastName] = useState("");
+  const [email, setEmail] = useState("");
 
-  const handleCurrentPasswordChange = (_event: React.FormEvent<HTMLInputElement>, value: string) => {
-    setCurrentPassword(value);
+  const handleFirstNameChange = (_event: React.FormEvent<HTMLInputElement>, value: string) => {
+    setFirstName(value);
   };
 
-  const handleNewPasswordChange = (_event: React.FormEvent<HTMLInputElement>, value: string) => {
-    setNewPassword(value);
+  const handleLastNameChange = (_event: React.FormEvent<HTMLInputElement>, value: string) => {
+    setLastName(value);
   };
-
-  const handleConfirmNewPasswordChange = (_event: React.FormEvent<HTMLInputElement>, value: string) => {
-    setConfirmNewPassword(value);
-  };
-
-  const onUpdateButtonClick = async (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    event.preventDefault();
-
-    setShowHelperText(false);
-
-    if (isPasswordForm) {
-      // Validate new password and confirm new password
-      setIsValidNewPassword(!!newPassword);
-      setIsValidConfirmNewPassword(newPassword === confirmNewPassword);
-
-      if (!newPassword || !confirmNewPassword || newPassword !== confirmNewPassword) {
-        setShowHelperText(true);
-        return;
-      }
-
-      // Call API endpoint to update password
-      try {
-        // Your API call for updating password
-        alert("Password update logic would go here.");
-      } catch (error: any) {
-        console.error("An unexpected error happened:", error);
-        setShowHelperText(true);
-      } finally {
-        setCurrentPassword("");
-        setNewPassword("");
-        setConfirmNewPassword("");
-      }
-    } else {
-      // Logic for updating general information
-      // Similar to your existing implementation
-      // You can handle it here or call another function to handle
-    }
+  const handleEmailChange = (_event: React.FormEvent<HTMLInputElement>, value: string) => {
+    setEmail(value);
   };
 
   return (
     <Card className="updateForm">
-      <CardTitle component="h1">{isPasswordForm ? "Change Password" : "Update Account"}</CardTitle>
+      <CardTitle component="h1">Update Account</CardTitle>
       <CardBody>
         {showHelperText && (
-          <HelperText>
-            <HelperTextItem variant="error" hasIcon icon={<ExclamationCircleIcon />}>
-              {isPasswordForm ? "Password and Confirm Password must match." : "Please fill out all fields."}
-            </HelperTextItem>
-          </HelperText>
+          <div>Error: Please fill out all fields.</div>
         )}
-
-        {isPasswordForm && (
-          <>
-            <TextInput
-              aria-label="currentPassword"
-              placeholder="Current Password"
-              name="currentPassword"
-              type="password"
-              value={currentPassword}
-              onChange={handleCurrentPasswordChange}
-              isRequired
-              className="update_currentPassword_input"
-            />
-            <TextInput
-              aria-label="newPassword"
-              placeholder="New Password"
-              name="newPassword"
-              type="password"
-              value={newPassword}
-              onChange={handleNewPasswordChange}
-              isRequired
-              validated={isValidNewPassword ? ValidatedOptions.default : ValidatedOptions.error}
-              className="update_newPassword_input"
-            />
-            <TextInput
-              aria-label="confirmNewPassword"
-              placeholder="Confirm New Password"
-              name="confirmNewPassword"
-              type="password"
-              value={confirmNewPassword}
-              onChange={handleConfirmNewPasswordChange}
-              isRequired
-              validated={isValidConfirmNewPassword ? ValidatedOptions.default : ValidatedOptions.error}
-              className="update_confirmNewPassword_input"
-            />
-          </>
-        )}
-
-        {/* Add other form fields for general information update if needed */}
-
+        <TextInput
+          aria-label="firstName"
+          name="firstName"
+          placeholder={user.firstname}
+          onChange={handleFirstNameChange}
+        />
+        <TextInput
+          aria-label="lastName"
+          name="lastName"
+          placeholder={user.lastname}
+          onChange={handleLastNameChange}
+        />
+        <TextInput
+          aria-label="email"
+          name="email"
+          placeholder={user.email}
+          onChange={handleLastNameChange}
+        />
         <ActionList>
           <ActionListItem>
-            <Button onClick={onUpdateButtonClick}>{isPasswordForm ? "Change Password" : "Update Account"}</Button>
-          </ActionListItem>
-          <ActionListItem>
-            <Link href={isPasswordForm ? "/user/account/update" : "login"}>
-              <Button>{isPasswordForm ? "Back to Account" : "Back to Login"}</Button>
-            </Link>
+            {/* <Button onClick={onUpdateButtonClick}>Update Account</Button> */}
           </ActionListItem>
         </ActionList>
       </CardBody>
