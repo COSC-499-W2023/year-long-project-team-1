c37e9f0f436e165bca702a37aaa5b1ebca8574d5
Thuan Vo <thuan.votann@gmail.com>
2023-11-30 21:04:22 -0800

    fixup(instrumentation): fix broken start up event'

--- app/web/src/instrumentation.ts
+++ app/web/src/instrumentation.ts
@@ -1,15 +1,16 @@
-import { createS3Bucket, getBucketName, isOwnedBucketExistException } from "@lib/s3";
-
 export async function register() {
-    const bucket = getBucketName();
-    try {
-        const resp = await createS3Bucket(bucket);
-        console.log(`Bucket ${resp.Location} created.`);
-    } catch (e) {
-        if (isOwnedBucketExistException(e)) {
-            console.log(`Bucket ${bucket} already exists. Nothing changed.`);
-            return;
+    if (process.env.NEXT_RUNTIME === "nodejs") {
+        const { createS3Bucket, getBucketName, isOwnedBucketExistException } = await import("@lib/s3");
+        const bucket = getBucketName();
+        try {
+            const resp = await createS3Bucket(bucket);
+            console.log(`Bucket ${resp.Location} created.`);
+        } catch (e) {
+            if (isOwnedBucketExistException(e)) {
+                console.log(`Bucket ${bucket} already exists. Nothing changed.`);
+                return;
+            }
+            throw e;
         }
-        throw e;
     }
 }
