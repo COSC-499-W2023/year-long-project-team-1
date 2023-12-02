eb8750ce57d9e921e1f8f21cc09e874b4e86bd6d
Connor Doman <domanc@student.ubc.ca>
2023-12-02 00:23:48 -0800

    feat(intregration): redirect from upload to review

---
+++ app/web/src/app/upload/review/[id]/page.tsx
@@ -0,0 +1,12 @@
+/*
+ * Created on Sun Nov 26 2023
+ * Author: Connor Doman
+ */
+
+export default async function VideoReviewPage({ params }: { params: { id: string } }) {
+    return (
+        <main>
+            <p>Video review page.</p>
+        </main>
+    );
+}
