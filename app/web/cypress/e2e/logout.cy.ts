3ab83306884ae915e7f235df4621a9b3a0201c07
Connor Doman <domanc@student.ubc.ca>
2023-12-03 18:27:11 -0800

    feat(tests): /logout tests

---
+++ app/web/cypress/e2e/logout.cy.ts
@@ -0,0 +1,33 @@
+describe("Logout page", () => {
+    beforeEach(() => {
+        cy.visit("/login");
+        cy.get("input[name=email]").type("johnny@example.com");
+        cy.get("input[name=password]").type("password");
+        cy.get("button[type=submit]").click();
+        cy.wait(250);
+    });
+
+    it("should redirect a logged out user to login", () => {
+        cy.visit("/logout");
+        cy.wait(250);
+        cy.url().should("include", "/login");
+    });
+
+    it("should clear a user session", () => {
+        cy.url().should("match", /\/$/);
+        cy.visit("/logout");
+        cy.wait(250);
+        cy.visit("/user/dashboard");
+        cy.wait(250);
+        cy.url().should("include", "/login");
+    });
+
+    it("should redirect to the provided redirect url", () => {
+        cy.visit("/logout?r=%2Fuser");
+        cy.wait(250);
+        cy.url().should("include", "/login");
+        cy.visit("/user/dashboard");
+        cy.wait(250);
+        cy.url().should("include", "/login");
+    });
+});
