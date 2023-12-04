8218455d8b09105359e0076e812bf491026f8294
Connor Doman <domanc@student.ubc.ca>
2023-12-03 20:33:08 -0800

    fix(test): improve /logout testing

--- app/web/cypress/e2e/logout.cy.ts
+++ app/web/cypress/e2e/logout.cy.ts
@@ -1,26 +1,23 @@
 describe("Logout page", () => {
-    beforeEach(() => {
-        cy.visit("/login");
-        cy.get("input[name=email]").type("johnny@example.com");
-        cy.get("input[name=password]").type("password");
-        cy.get("button[type=submit]").click();
-        cy.wait(250);
-    });
+    beforeEach(() => {});
 
     it("should redirect a logged out user to login", () => {
-        cy.visit("/logout");
-        cy.wait(250);
         cy.visit("/logout");
         cy.wait(250);
         cy.url().should("include", "/login");
     });
 
     it("should clear a user session", () => {
+        cy.visit("/login");
+        cy.get("input[name=email]").type("johnny@example.com");
+        cy.get("input[name=password]").type("password");
+        cy.get("button[type=submit]").click();
+        cy.wait(250);
         cy.url().should("match", /\/$/);
         cy.visit("/logout");
         cy.wait(250);
         cy.visit("/user/dashboard");
-        cy.wait(250);
+        cy.wait(500);
         cy.url().should("include", "/login");
     });
 });
