2c064af5c99ba171a27273b02c2c021aabe14401
Connor Doman <domanc@student.ubc.ca>
2023-12-03 19:02:09 -0800

    feat(tests): /staff page tests

---
+++ app/web/cypress/e2e/staff/page.cy.ts
@@ -0,0 +1,28 @@
+describe("Staff page rendering", () => {
+    beforeEach(() => {
+        cy.visit("/staff");
+        cy.wait(250);
+        cy.get("input[name=email]").type("johnny@example.com");
+        cy.get("input[name=password]").type("password");
+        cy.get("button[type=submit]").click();
+        cy.wait(250);
+    });
+
+    it("Should show the staff page", () => {
+        cy.get("[aria-label='Staff-only page']").contains("Staff");
+    });
+
+    it("Should show a sample list of users", () => {
+        const usersList = cy.get("[aria-label='Example user list']");
+        usersList.should("exist");
+        usersList.get("h2").should("contain", "Users List");
+    });
+});
+
+describe("Staff page functionality", () => {
+    it("Should redirect to /login if not logged in", () => {
+        cy.visit("/staff");
+        cy.wait(250);
+        cy.url().should("include", "/login");
+    });
+});
