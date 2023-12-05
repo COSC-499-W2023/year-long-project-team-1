bae00235caba5bc2665703e8feb02d5943cbe506
Connor Doman <domanc@student.ubc.ca>
2023-12-04 23:05:45 -0800

    feat(video-review): add video review UI (#190)

---
+++ app/web/src/components/VideoReview.tsx
@@ -0,0 +1,66 @@
+/*
+ * Created on Mon Nov 27 2023
+ * Author: Connor Doman
+ */
+"use client";
+
+import { ActionList, ActionListItem, Button, Card, CardBody, CardTitle } from "@patternfly/react-core";
+import { CheckIcon, TimesIcon } from "@patternfly/react-icons";
+import Link from "next/link";
+import style from "@assets/style";
+import { useRouter } from "next/navigation";
+
+export const videoReviewStyle = {
+    ...style,
+    videoPlayer: {
+        width: "100%",
+        aspectRatio: "16 / 9",
+        backgroundColor: "black",
+        margin: "1rem 0",
+        borderRadius: "0.25rem",
+    },
+};
+
+interface VideoReviewProps {
+    videoId: string;
+}
+
+export const VideoReview = ({ videoId }: VideoReviewProps) => {
+    const router = useRouter();
+
+    const videoFilename = videoId.replace(".mp4", "") + "-processed.mp4";
+
+    const handleAccept = () => {
+        // TODO: upload video to S3 with Server Action
+        alert("TODO: upload video to S3 with Server Action");
+    };
+
+    const handleReject = () => {
+        router.push("/upload");
+    };
+
+    return (
+        <Card style={style.card}>
+            <CardTitle component="h1">Review Your Submission</CardTitle>
+            <CardBody>
+                <video controls autoPlay={false} style={videoReviewStyle.videoPlayer}>
+                    <source src={`/api/video/processed?file=${videoFilename}`} />
+                </video>
+                <ActionList style={style.actionList}>
+                    <ActionListItem>
+                        <Button icon={<CheckIcon />} onClick={() => handleAccept()}>
+                            This looks good
+                        </Button>
+                    </ActionListItem>
+                    <ActionListItem>
+                        <Button variant="danger" icon={<TimesIcon />} onClick={() => handleReject()}>
+                            Cancel
+                        </Button>
+                    </ActionListItem>
+                </ActionList>
+            </CardBody>
+        </Card>
+    );
+};
+
+export default VideoReview;
