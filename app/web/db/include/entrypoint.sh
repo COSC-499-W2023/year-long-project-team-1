62bb6dbdd7a288cab4ee88a02106c7e04adb0a89
Thuan Vo <thuan.votann@gmail.com>
2023-12-05 16:46:32 -0800

    chore(license): add project license
    Signed-off-by: Thuan Vo <thuan.votann@gmail.com>

--- app/web/db/include/entrypoint.sh
+++ app/web/db/include/entrypoint.sh
@@ -1,4 +1,18 @@
 #!/usr/bin/env bash
+# Copyright [2023] [Privacypal Authors]
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 
 set -e
 
@@ -22,7 +36,7 @@ generate_user_rows() {
         echo "[SEVERE][$(date)]: ${PRIVACYPAL_USER_PROPERTY_PATH} does not exist." && exit 1
     fi
 
-    bash "$DIR/generate_insertion.bash" -f "${PRIVACYPAL_USER_PROPERTY_PATH}" -t User | prisma db execute --schema schema.prisma --stdin
+    bash "$DIR/generate_insertion.sh" -f "${PRIVACYPAL_USER_PROPERTY_PATH}" -t User | prisma db execute --schema schema.prisma --stdin
 }
 
 # Prune user rows if necessary
