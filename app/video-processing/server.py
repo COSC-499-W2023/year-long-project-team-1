6b7431013dd6b415095612a11003d439e027f742
MyStackOverflows <piunger@hotmail.com>
2023-12-06 15:29:26 -0800

    feat(back-end): remove 'stateless' mode for vidproc server

--- app/video-processing/server.py
+++ app/video-processing/server.py
@@ -16,7 +16,7 @@ import os
 import multiprocessing as mp
 from video_processor import VideoProcessor
 from process_tracker import ProcessTrackerObject, ProcessTracker
-from quart import Quart, request, jsonify, utils
+from quart import Quart, request, jsonify
 from utils import get_env
 import logging
 import signal
@@ -50,55 +50,42 @@ async def handle_request():
         return "Error: file not found", 404
 
     dest_path = f"{output_path}/{file[:-4]}-processed{file[-4:]}"
-    if not app.config["IS_STATELESS"]:    # start process and send response immediately
-        process = mp.Process(target=start_process, args=(file_path, dest_path))
-        tracker: ProcessTracker = app.config["TRACKER"]
-        tracker.add(file, ProcessTrackerObject(process))
-        process.start()  # start the process on another thread
-        LOGGER.info(f"Processing started on {file}")
-        return "Success: file exists.", 202         # indicate processing has started
-    else:
-        LOGGER.info(f"Process started on {file}")
-        response = await utils.run_sync(start_process)(file, dest_path)
-        return response
+    process = mp.Process(target=start_process, args=(file_path, dest_path))
+    tracker: ProcessTracker = app.config["TRACKER"]
+    tracker.add(file, ProcessTrackerObject(process))
+    process.start()  # start the process on another thread
+    LOGGER.info(f"Processing started on {file}")
+    return "Success: file exists.", 202         # indicate processing has started
 
 
 @app.route("/process_status", methods=["GET"])
 async def return_status():
-    if not app.config["IS_STATELESS"]:  # only enable this route if we're running in stateless mode
-        tracker: ProcessTracker = app.config["TRACKER"]
-        process = tracker.get_process(request.args["filename"])
-        if process is None:
-            return "Process does not exist", 404  # shouldn't ever happen, but just in case
-        return f"{not process.is_alive()}", 200
-    else:
-        LOGGER.warning("Not running in stateless mode, returning 501")
-        return "", 501
+    tracker: ProcessTracker = app.config["TRACKER"]
+    process = tracker.get_process(request.args["filename"])
+    if process is None:
+        return "Process does not exist", 404  # shouldn't ever happen, but just in case
+    return f"{not process.is_alive()}", 200
 
 
 @app.route("/cancel_process", methods=["POST"])
 async def cancel_process():
-    if not app.config["IS_STATELESS"]:
-        file = (await request.data).decode()    # get filename from request
+    file = (await request.data).decode()    # get filename from request
 
-        # terminate the process
-        tracker: ProcessTracker = app.config["TRACKER"]
-        process: ProcessTrackerObject = tracker.get_process(file)
-        if process is None:     # process doesn't exist/isn't running/has been pruned
-            return "Process does not exist in the current runtime", 404
+    # terminate the process
+    tracker: ProcessTracker = app.config["TRACKER"]
+    process: ProcessTrackerObject = tracker.get_process(file)
+    if process is None:     # process doesn't exist/isn't running/has been pruned
+        return "Process does not exist in the current runtime", 404
 
-        process.terminate()
+    process.terminate()
 
-        # cleanup files that may or may not exist as a result of cancelling a video processing operation
-        for f in [f"{app.config['OUTPUT_DIR']}/{file[:-4]}-processed{file[-4:]}",
-                  f"{app.config['OUTPUT_DIR']}/{file[:-4]}-processed-temp{file[-4:]}"]:
-            if os.path.isfile(f):
-                os.remove(f)
+    # cleanup files that may or may not exist as a result of cancelling a video processing operation
+    for f in [f"{app.config['OUTPUT_DIR']}/{file[:-4]}-processed{file[-4:]}",
+              f"{app.config['OUTPUT_DIR']}/{file[:-4]}-processed-temp{file[-4:]}"]:
+        if os.path.isfile(f):
+            os.remove(f)
 
-        return "Success", 200
-    else:
-        LOGGER.warning("Not running in stateless mode, returning 501")
-        return "", 501
+    return "Success", 200
 
 
 @app.route("/health", methods=["GET"])
@@ -113,7 +100,6 @@ async def before_all():
     app.config["PROCESSOR"] = VideoProcessor.get_instance()
     app.config["INPUT_DIR"] = get_env("PRIVACYPAL_INPUT_VIDEO_DIR", "/opt/privacypal/input_videos")
     app.config["OUTPUT_DIR"] = get_env("PRIVACYPAL_OUTPUT_VIDEO_DIR", "/opt/privacypal/output_videos")
-    app.config["IS_STATELESS"] = get_env("PRIVACYPAL_IS_STATELESS", "false").lower() == "true"
     app.config["ENVIRONMENT"] = get_env("ENVIRONMENT", "production")
 
     tracker: ProcessTracker = app.config["TRACKER"]
