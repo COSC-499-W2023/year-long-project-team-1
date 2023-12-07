1a8d3400427c8b892edc1d8dba73cb2451c78ff5
nganphan123 <jill01009@gmail.com>
2023-12-06 20:59:41 -0800

    add user api test

--- app/web/cypress/e2e/api/users.cy.ts
+++ app/web/cypress/e2e/api/users.cy.ts
@@ -15,12 +15,69 @@
  */
 
 describe("/api/users: Fetch users", () => {
-  it("Should return a list of users", () => {
-    cy.request("/api/users").then((response) => {
-      expect(response.status).to.eq(200);
-      expect(response.body).to.have.property("result");
-      expect(Array.isArray(response.body.result.data)).to.be.true;
-      expect(response.body.result.data.length).to.be.at.least(0);
+    const verifySuccessRespBody = (response: Cypress.Response<any>) => {
+        expect(response.status).to.eq(200);
+        expect(response.body).to.have.property("result");
+        expect(Array.isArray(response.body.result.data)).to.be.true;
+    }
+
+    context("GET /api/users?id=<mockid>", function(){
+        it("gets user based on id", function(){
+            cy.request({
+                method: "GET", 
+                url: "/api/users", 
+                qs:{id: 1},
+            }).then((response) => {
+                verifySuccessRespBody(response);
+                expect(response.body.result.data.length).to.eq(1);
+                })
+        }); 
+
+        it("error when invalid id", function(){
+            cy.request({
+                method: "GET", 
+                url: "/api/users", 
+                qs:{id: "abc"},
+                failOnStatusCode: false,
+            }).then((response) => {
+                expect(response.status).to.eq(400);
+                expect(response.body.errors[0].detail).to.eq("id must be a list of string numbers or single string number.");
+            })
+        }); 
     });
-  });
+
+    context("GET /api/users?id=<mockids>", function(){
+        it("gets user based on list of ids", function(){
+            cy.request({
+                method: "GET", 
+                url: "/api/users", 
+                qs:{id: "1,2,3"},
+            }).then((response) => {
+                verifySuccessRespBody(response);
+                expect(response.body.result.data.length).to.be.at.least(1);
+            })
+        });
+        it("gets error if one of id is invalid", function(){
+            cy.request({
+                method: "GET", 
+                url: "/api/users", 
+                qs:{id: "1,abc,3"},
+                failOnStatusCode: false,
+            }).then((response) => {
+                expect(response.status).to.eq(400);
+                expect(response.body.errors[0].detail).to.eq("id must be a list of string numbers or single string number.");
+            })
+        }); 
+     });
+
+    context("GET /api/users", function(){
+    it("gets list of all users", () => {
+        cy.request("/api/users").then((response) => {
+            verifySuccessRespBody(response);
+            expect(response.body.result.data.length).to.be.at.least(0);
+        });
+        }); 
+    });
+
+    // TODO: add PUT test
 });
