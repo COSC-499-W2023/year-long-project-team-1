62743e975701ed6fee716aa68040bb7c758afe92
MyStackOverflows <piunger@hotmail.com>
2024-01-18 13:51:13 -0800

    feat(lambda): update ECR push script to include login

--- app/video-processing/lambda/push.sh
+++ app/video-processing/lambda/push.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+aws ecr get-login-password --region ca-central-1 | docker login --username AWS --password-stdin 063327155631.dkr.ecr.ca-central-1.amazonaws.com
 docker build --platform linux/amd64 -t vidproc-lambda .
 docker tag vidproc-lambda:latest 063327155631.dkr.ecr.ca-central-1.amazonaws.com/vidproc-lambda:latest
 docker push 063327155631.dkr.ecr.ca-central-1.amazonaws.com/vidproc-lambda:latest
