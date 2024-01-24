cef1752505a19bed660e0a78637d5af55b5181c7
MyStackOverflows <piunger@hotmail.com>
2024-01-24 14:15:49 -0800

    feat(api): initial untested modifications

--- app/web/src/app/api/video/upload/route.ts
+++ app/web/src/app/api/video/upload/route.ts
@@ -20,6 +20,8 @@ import { NextResponse } from "next/server";
 import fs from "fs/promises";
 import { getSession } from "@lib/session";
 import { RESPONSE_NOT_AUTHORIZED } from "@lib/response";
+import { S3Client, PutObjectCommand, S3ClientConfig } from "@aws-sdk/client-s3";
+import { useState } from "react";
 
 const videosDirectory =
   process.env.PRIVACYPAL_INPUT_VIDEO_DIR || "/opt/privacypal/input_videos";
@@ -62,10 +64,11 @@ export async function POST(req: Request) {
     );
   }
 
-  let filename: string;
-  let filePath: string;
+    let filename: string;
+  //   let filePath: string;
   try {
-    [filename, filePath] = await saveVideo(file, userID);
+    // [filename, filePath] = await saveVideo(file, userID);
+    filename = await s3Upload(file, userID);
   } catch (err: any) {
     return Response.json(
       { message: "Internal Server Error: " + err.message },
@@ -73,19 +76,24 @@ export async function POST(req: Request) {
     );
   }
 
-  const videoServerRes = await postToVideoServer(filename);
+  return Response.json(
+    { data: { success: true, filePath: filename } },
+    { status: 200 },
+  );
 
-  if (videoServerRes.status == 202) {
-    return Response.json(
-      { data: { success: true, filePath: filename } },
-      { status: 200 },
-    );
-  } else {
-    await fs.unlink(filePath);
-    /* Although the error comes from python server, 
-           it's inside the system as a whole so return internal sever error instead*/
-    return Response.json({ message: "Internal Server Error" }, { status: 500 });
-  }
+  //   const videoServerRes = await postToVideoServer(filename);
+  //
+  //   if (videoServerRes.status == 202) {
+  //     return Response.json(
+  //       { data: { success: true, filePath: filename } },
+  //       { status: 200 },
+  //     );
+  //   } else {
+  //     await fs.unlink(filePath);
+  //     /* Although the error comes from python server,
+  //            it's inside the system as a whole so return internal sever error instead*/
+  //     return Response.json({ message: "Internal Server Error" }, { status: 500 });
+  //   }
 }
 
 async function postToVideoServer(filename: string): Promise<Response> {
@@ -138,4 +146,49 @@ async function saveVideo(
     console.error(err);
     throw err;
   }
+}
+
+async function s3Upload(file: File, userID: string): Promise<string> {
+  // read the file into a buffer
+  const bytes = await file.arrayBuffer();
+  const buffer = Buffer.from(bytes);
+
+  // determine the path to write the file to
+  const cwd = videosDirectory;
+  const extension = path.extname(file.name);
+  // file name extracted from request
+  const fileBaseName = path.basename(file.name, extension);
+  // file name combined with userID and timestamp
+  const filename = `${userID}-${fileBaseName}-${timeStampUTC()}${extension}`;
+  const filePath = path.join(cwd, filename);
+
+  // upload to s3
+  const keyId = process.env.AWS_ACCESS_KEY_ID || "";
+  const accessKey = process.env.AWS_SECRET_ACCESS_KEY || "";
+  if (keyId == "" || accessKey == "") {
+    // make sure keyId and accessKey are properly defined
+    var msg = "Environment variables not defined";
+    console.error(msg);
+    throw msg;
+  }
+  const client = new S3Client({
+    credentials: {
+      accessKeyId: keyId,
+      secretAccessKey: accessKey,
+    },
+    region: "ca-central-1",
+  });
+  const putCommand = new PutObjectCommand({
+    Bucket: "privacypal-input",
+    Key: filename,
+    Body: buffer,
+  });
+  try {
+    const response = await client.send(putCommand);
+    console.log(response);
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+  return filename;
 }
