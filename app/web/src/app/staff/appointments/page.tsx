ef590d55b67bcd2691c5ea2c848483698b48ec04
MyStackOverflows <piunger@hotmail.com>
2024-01-25 17:18:13 -0800

    feat(front-end): format with Prettier v2

--- app/web/src/app/staff/appointments/page.tsx
+++ app/web/src/app/staff/appointments/page.tsx
@@ -43,5 +43,5 @@ export default async function ViewAppointmentDetailsForm() {
     appointments.push(<AppointmentViewer appointment={appt} viewer={user} />);
   });
 
-  return (<main>{appointments}</main>);
+  return <main>{appointments}</main>;
 }
