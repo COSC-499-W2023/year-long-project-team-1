f62deb1e88feb38bf8f7e75c9cca956e25bc39d7
MyStackOverflows <piunger@hotmail.com>
2024-01-28 16:39:19 -0800

    feat(api): fix prettier

--- app/web/src/lib/s3.ts
+++ app/web/src/lib/s3.ts
@@ -33,11 +33,11 @@ export async function getRegion() {
 }
 
 export function getOutputBucket() {
-    return process.env.PRIVACYPAL_OUTPUT_BUCKET || 'privacypal-output';
+  return process.env.PRIVACYPAL_OUTPUT_BUCKET || "privacypal-output";
 }
 
 export function getTmpBucket() {
-    return process.env.PRIVACYPAL_TMP_BUCKET || 'privacypal-input';
+  return process.env.PRIVACYPAL_TMP_BUCKET || "privacypal-input";
 }
 
 export function isOwnedBucketExistException(
