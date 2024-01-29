17b7a2f42a9fe33532b89b2246e8028704593d32
MyStackOverflows <piunger@hotmail.com>
2024-01-28 22:48:21 -0800

    feat(api): refactor

--- app/web/src/app/api/video/upload/route.ts
+++ app/web/src/app/api/video/upload/route.ts
@@ -18,7 +18,7 @@ import { timeStampUTC } from "@lib/time";
 import { NextResponse } from "next/server";
 import { getSession } from "@lib/session";
 import { RESPONSE_NOT_AUTHORIZED } from "@lib/response";
-import { client, getTmpBucket, uploadArtifactFromFileRef } from "@lib/s3";
+import { client, getTmpBucket, putArtifactFromFileRef } from "@lib/s3";
 import { PutObjectCommand } from "@aws-sdk/client-s3";
 import { buffer } from "stream/consumers";
 
@@ -58,26 +58,17 @@ export async function POST(req: Request) {
   }
 
   try {
-    const buffer = Buffer.from(await file.arrayBuffer());
     // determine the path to write the file to
     const extension = path.extname(file.name);
     // file name extracted from request
     const fileBaseName = path.basename(file.name, extension);
     // file name combined with userID and timestamp
     const filename = `${userID}-${fileBaseName}-${timeStampUTC()}${extension}`;
-    // This code block below causes issues with the lambda because of multipart upload (Thuan and Paul think)
-    // await uploadArtifactFromFileRef({
-    //   bucket: getTmpBucket(),
-    //   key: filename,
-    //   file: file,
-    // });
-    const putCommand = new PutObjectCommand({
-      Bucket: getTmpBucket(),
-      Key: filename,
-      Body: buffer,
+    await putArtifactFromFileRef({
+      bucket: getTmpBucket(),
+      key: filename,
+      file: file,
     });
-
-    await client.send(putCommand);
 
     return Response.json(
       { data: { success: true, filePath: filename } },
