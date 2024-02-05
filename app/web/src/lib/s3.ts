d85bb0a14d4d464aa2cca1255b556ac2ec1f4efc
Thuan Vo <thuan.votann@gmail.com>
2024-02-04 19:57:48 -0800

    fix(smoketest): update smoketest with new app changes (#458)
    Signed-off-by: Thuan Vo <thuan.votann@gmail.com>

--- app/web/src/lib/s3.ts
+++ app/web/src/lib/s3.ts
@@ -16,9 +16,9 @@
 
 import {
   S3Client,
+  HeadBucketCommand,
   PutObjectCommand,
   GetObjectCommand,
-  HeadBucketCommand,
 } from "@aws-sdk/client-s3";
 import { Upload } from "@aws-sdk/lib-storage";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
@@ -95,7 +95,7 @@ export async function uploadArtifactFromPath({
 export async function testS3Connection(): Promise<boolean> {
   try {
     const buckets = [
-      process.env.PRIVACYPAL_S3_BUCKET,
+      process.env.PRIVACYPAL_OUTPUT_BUCKET,
       process.env.PRIVACYPAL_TMP_BUCKET,
     ];
     buckets.forEach(async (bucket) => {
