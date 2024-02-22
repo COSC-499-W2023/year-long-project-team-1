271ae157da46b1537e0f180e8c00ecd192b52224
Paul Unger <44368989+MyStackOverflows@users.noreply.github.com>
2024-02-21 22:22:17 -0800

    feat(appointment): add appointment management UI (#395)
    Signed-off-by: Paul Unger <44368989+MyStackOverflows@users.noreply.github.com>

---
+++ app/web/src/components/appointment/AppointmentManagementList.tsx
@@ -0,0 +1,62 @@
+/*
+ * Copyright [2023] [Privacypal Authors]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+"use client";
+
+import { useEffect, useState } from "react";
+import { getAllProfessionalAppointmentDetails } from "@app/actions";
+import AppointmentViewer from "./AppointmentViewer";
+import { CognitoUser } from "@lib/cognito";
+import { User } from "next-auth";
+
+interface AppointmentManagementListProps {
+  professional: User;
+}
+
+export default function AppointmentManagementList({
+  professional,
+}: AppointmentManagementListProps) {
+  const [appointments, setAppointments] = useState<JSX.Element[]>([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    getAllProfessionalAppointmentDetails(professional).then((i) => {
+      let componentList: JSX.Element[] = [];
+      i.forEach((j, count) => {
+        componentList.push(
+          <AppointmentViewer
+            appointment={j}
+            viewer={professional}
+            key={count}
+          />,
+        );
+      });
+      setAppointments(componentList);
+      setLoading(false);
+    });
+  }, []);
+
+  return (
+    <main>
+      {loading ? (
+        <div>Loading...</div>
+      ) : appointments.length > 0 ? (
+        appointments
+      ) : (
+        "No appointments found."
+      )}
+    </main>
+  );
+}
---
+++ app/web/src/components/appointment/AppointmentViewer.tsx
@@ -0,0 +1,93 @@
+/*
+ * Copyright [2023] [Privacypal Authors]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+"use client";
+
+import LinkButton from "@components/form/LinkButton";
+import { ViewableAppointment } from "@lib/appointment";
+import { UserRole } from "@lib/userRole";
+import { Button, Card, CardBody } from "@patternfly/react-core";
+import { User } from "next-auth";
+
+interface AppointmentViewerProps {
+  appointment: ViewableAppointment;
+  viewer?: User;
+}
+
+export default function AppointmentViewer({
+  appointment,
+  viewer,
+}: AppointmentViewerProps) {
+  const proUser =
+    appointment.professionalUser.firstName +
+    " " +
+    appointment.professionalUser.lastName;
+  const clientUser =
+    appointment.clientUser.firstName + " " + appointment.clientUser.lastName;
+
+  const viewerRole = viewer?.role;
+
+  const uploadButton =
+    viewerRole === UserRole.CLIENT ? (
+      <LinkButton href="/upload" label="Upload a video to this appointment" />
+    ) : null;
+
+  const cancelButton =
+    viewerRole === UserRole.PROFESSIONAL ? (
+      <Button
+        onClick={async () => {
+          // make a DELETE request to the appointments api to delete the appointment
+          const response = await fetch(
+            `/api/appointments?id=${appointment.id}`,
+            {
+              method: "DELETE",
+            },
+          );
+          if (response.status == 200) {
+            alert("Successfully deleted appointment.");
+          } else {
+            alert("An error occurred while deleting appointment.");
+          }
+        }}
+        variant="danger"
+      >
+        Cancel appointment
+      </Button>
+    ) : null;
+
+  const viewDetailsButton =
+    viewerRole === UserRole.PROFESSIONAL ? (
+      <LinkButton
+        href={`/staff/appointment?id=${appointment.id}`}
+        label="View appointment details"
+      />
+    ) : null;
+
+  return (
+    <Card>
+      <CardBody>
+        <h2>Appointment</h2>
+        <time>{appointment.time.toLocaleDateString("en-US")}</time>
+        <p>Professional: {proUser}</p>
+        <p>Client: {clientUser}</p>
+        <p>Number of associated videos: {appointment.video_count}</p>
+        {uploadButton}
+        {cancelButton}
+        {viewDetailsButton}
+      </CardBody>
+    </Card>
+  );
+}
