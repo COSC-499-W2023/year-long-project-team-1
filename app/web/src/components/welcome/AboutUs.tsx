216c341463bed6890eb7d75e5cdff089c9f82bc2
linhnnk <linhnnk202@gmail.com>
2024-03-23 14:40:07 -0700

    home page has welcome component

--- app/web/src/components/welcome/AboutUs.tsx
+++ app/web/src/components/welcome/AboutUs.tsx
@@ -1,3 +1,18 @@
+/*
+ * Copyright [2023] [Privacypal Authors]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import React from "react";
 import { CSS } from "@lib/utils";
 import LinkButton from "@components/form/LinkButton";
@@ -67,17 +82,22 @@ const AboutUs: React.FC = () => {
         <h1 style={text1}>A SOLUTION TO ABSOLUTE </h1>
         <h1 style={text2}>PRIVACY.</h1>
       </div>
+      <br />
       <div style={aboutUsContainer}>
         <h1 style={aboutUsText}>
           Welcome to PrivacyPal, where your health and privacy are our top
           priorities. We understand that your personal health information is
-          sensitive and deserves the highest level of protection. Our service is
-          designed with your privacy in mind, ensuring that your medical data
-          and personal details are safeguarded at all times.
+          sensitive and deserves the highest level of protection.
+        </h1>
+        <br />
+        <h1 style={aboutUsText}>
+          Our service is designed with your privacy in mind, ensuring that your
+          medical data and personal details are safeguarded at all times.
         </h1>
       </div>
+      <br />
       <div style={buttonContainer}>
-        <LinkButton href="/staff" label="Get Started" />
+        <LinkButton href="/user" label="Get Started" />
       </div>
     </div>
   );
--- app/web/src/components/welcome/GifImage.tsx
+++ app/web/src/components/welcome/GifImage.tsx
@@ -1,3 +1,18 @@
+/*
+ * Copyright [2023] [Privacypal Authors]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import React from "react";
 import { CSS } from "@lib/utils";
 import gifimage from "@assets/welcome.gif";
@@ -5,7 +20,7 @@ import Image from "next/image";
 
 const gifContainerStyle: CSS = {
   position: "absolute",
-  top: "90%",
+  top: "80%",
   left: "30%",
   transform: "translate(-75%, -75%)",
 };
