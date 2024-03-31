9a0279bc17433d4dd8b2b8b86d414587c3ee9496
Connor Doman <domanc@student.ubc.ca>
2024-03-31 11:56:04 -0700

    fix(video-review): use front-end based redirect after success

--- app/web/src/components/VideoReview.tsx
+++ app/web/src/components/VideoReview.tsx
@@ -31,6 +31,8 @@ import { useEffect, useState } from "react";
 import Loading from "@app/loading";
 import LoadingButton from "./form/LoadingButton";
 import { User } from "next-auth";
+import { useRouter } from "next/navigation";
+import { getUserHubSlug } from "@lib/utils";
 
 export const videoReviewStyle = {
   ...style,
@@ -50,6 +52,8 @@ interface VideoReviewProps {
 }
 
 export const VideoReview = ({ videoId, user, apptId }: VideoReviewProps) => {
+  const router = useRouter();
+
   const [actionMessage, setActionMessage] = useState<string>("");
   const [isError, setIsError] = useState<boolean>(false);
 
@@ -74,6 +78,7 @@ export const VideoReview = ({ videoId, user, apptId }: VideoReviewProps) => {
       .then((res) => {
         if (res.ok) {
           setActionMessage(successMsg);
+          router.push(`${getUserHubSlug(user)}/appointments`);
         } else {
           setActionMessage(errorMsg);
           setIsError(true);
@@ -85,9 +90,6 @@ export const VideoReview = ({ videoId, user, apptId }: VideoReviewProps) => {
         setActionMessage(errorMsg);
         setIsError(true);
       });
-    setTimeout(() => {
-      redirectAfterReview(user);
-    }, 2000);
   };
 
   const getHandler = (action: string) => {
