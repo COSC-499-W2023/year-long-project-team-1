b6f36978a08385ca36ce57fa082f28c034143d30
Connor Doman <domanc@student.ubc.ca>
2024-03-31 18:30:36 -0700

    feat(appointments): improve video upload UX with Upload Wizard (#737)
    Co-authored-by: nganphan123 <jill01009@gmail.com>
    Co-authored-by: linhnnk <linhnnk202@gmail.com>
    Co-authored-by: MyStackOverflows <piunger@hotmail.com>
    Co-authored-by: Thuan Vo <thuan.votann@gmail.com>

---
+++ app/web/src/components/form/SelectedItem.tsx
@@ -0,0 +1,53 @@
+/*
+ * Copyright [2023] [Privacypal Authors]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+"use client";
+import { CSS } from "@lib/utils";
+import { Text } from "@patternfly/react-core";
+import { CheckIcon, TimesIcon } from "@patternfly/react-icons";
+
+const selectedItemStyle: CSS = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  gap: "0.5rem",
+};
+
+interface SelectedItemProps {
+  hide?: boolean;
+  selected?: boolean;
+  style?: CSS;
+  children?: React.ReactNode;
+}
+
+export const SelectedItem = ({
+  hide,
+  selected = true,
+  style,
+  children,
+}: SelectedItemProps) => {
+  if (hide) {
+    return null;
+  }
+
+  const icon = selected ? <CheckIcon /> : <TimesIcon />;
+
+  return (
+    <Text style={{ ...selectedItemStyle, ...style }}>
+      {icon}
+      {children}
+    </Text>
+  );
+};
---
+++ app/web/src/components/upload/FileUploader.tsx
@@ -0,0 +1,79 @@
+/*
+ * Copyright [2023] [Privacypal Authors]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { CSS } from "@lib/utils";
+import { Button, DropEvent, FileUpload } from "@patternfly/react-core";
+import { useRef, useState } from "react";
+
+interface FileUploaderProps {
+  acceptedFileTypes: string[];
+  existingFile?: File;
+  style?: CSS;
+  onUpload: (file: File) => void;
+  onClear?: () => void;
+}
+
+export const FileUploader = ({
+  acceptedFileTypes,
+  existingFile,
+  style,
+  onUpload,
+  onClear,
+}: FileUploaderProps) => {
+  const [value, setValue] = useState(existingFile);
+  const [filename, setFilename] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleFileInputChange = (_: DropEvent, file: File) => {
+    setFilename(file.name);
+    onUpload(file);
+  };
+
+  const handleClear = (
+    _event: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  ) => {
+    setFilename("");
+    setValue(undefined);
+
+    if (onClear) onClear();
+  };
+
+  const handleFileReadStarted = (_event: DropEvent, _fileHandle: File) => {
+    setIsLoading(true);
+  };
+
+  const handleFileReadFinished = (_event: DropEvent, _fileHandle: File) => {
+    setIsLoading(false);
+  };
+
+  return (
+    <FileUpload
+      id="text-file-simple"
+      value={value}
+      filename={filename}
+      filenamePlaceholder="Drag and drop a file or upload one"
+      onFileInputChange={handleFileInputChange}
+      onReadStarted={handleFileReadStarted}
+      onReadFinished={handleFileReadFinished}
+      onClearClick={handleClear}
+      isLoading={isLoading}
+      browseButtonText="Upload"
+      dropzoneProps={{
+        accept: { "video/*": acceptedFileTypes },
+      }}
+      style={style}
+    />
+  );
+};
